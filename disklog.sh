74ca2fdf92b950d97a5d8d11a4097fd657e20e3a
minghu6 <a19678zy@163.com>
2016-02-11 20:07:54 -0500

    new .sh file

---
+++ disklog.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+#filename disklog.sh
+
+#use listen disk usage on remote machine
+
+
+logfile="/var/log/diskusage.log"
+
+if [[ -n $1 ]]
+then
+    logfile=$1
+fi
+
+if [ ! -e $logfile ] #if-exist
+then
+    printf "%-8s %-14s %-9s %-8s %-6s %-6s %-6s %s\n" "Date" "IP address"\
+           "Device" "Capacity" "Used" "Free" "Percent" "Status" > $logfile
+fi
+
+IP_LIST="127.0.0.1 0.0.0.0"#remote ip list
+
+(
+    for ip in $IP_LIST;
+    do
+        #root username
+        ssh root@$ip 'df -H' | grep ^/dev/ > /tmp/$$.df
+
+        while read line;
+        do
+            cur_date=$(date +%D)
+            printf "%-8s %-14s" $cur_date $ip
+
+            echo $line | awk '{printf ("%-9s %-8s %-6s %-6s %-8s",\
+            $1 $2 $3 $4 $5);}'
+
+            pusg=$(echo $line | egrep -o "[0-9]+%")
+            pusg=$(pusg/\%/);
+
+            if [ $pusg -lt 80 ];
+            then
+                echo SAFE
+            else
+                echo ALERT
+            fi
+
+        done < /tmp/$$.df
+    done
+) >> $logfile
